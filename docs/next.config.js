af12a32afff87d359238f214abcda4e27f475651
Sven Huepers <sven.huepers@pwc.com>
2023-06-14 11:45:21 +0200

    initial commit docs w/ nextjs

---
+++ docs/next.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  output: 'export',
+  basePath: '/bachelorarbeit',
+  experimental: {
+    appDir: true,
+  },
+}
+
+module.exports = nextConfig
