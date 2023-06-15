0857f206e9523d2d92d8ca19c369ce13e23f58c9
Sven Huepers <sven.huepers@pwc.com>
2023-06-15 15:09:57 +0200

    Add docs/out back

--- app/src/components/Shared/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as PageHeader } from "./PageHeader";
-export { default as LoadingDots } from "./LoadingDots";
-export { default as Card } from "./Card";
-export { default as FileModal } from "./FileModal";
-export * from "./FileModal";
--- app/src/lib/redis.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "redis";
-
-const client = createClient({
-  url: process.env.REDIS_URL,
-  password: process.env.REDIS_PASSWORD,
-});
-client.connect();
-
-export default client;
